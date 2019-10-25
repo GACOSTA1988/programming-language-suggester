238f323f6c8cc62e6d38979cf1de2dfde1ce18e0
George <greg@gregs-MacBook-Pro.local>
2019-10-25 15:34:40 -0700

    troubleshoot javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
 $(document).ready(function(){
   $("form#formOne").submit(function(event){
-  event.preventDefault();
+    event.preventDefault();
 
-  var option1 = $("#option1").val();
-  var option2 = $("#option2").val();
-  var option3 = $("#option3").val();
-  var option4 = $("#option4").val();
-  var option5 = $("#option5").val();
+  var option1 = $("select#option1").val();
+  var option2 = $("select#option2").val();
+  var option3 = $("select#option3").val();
+  var option4 = $("select#option4").val();
+  var option5 = $("select#option5").val();
 
-  option1 = $("select#option1");
-  option2 = $("select#option2");
-  option3 = $("select#option3");
-  option4 = $("select#option4");
-  option5 = $("select#option5");
+  // option1 = $("select#option1");
+  // option2 = $("select#option2");
+  // option3 = $("select#option3");
+  // option4 = $("select#option4");
+  // option5 = $("select#option5");
 
-  if (option1 === "Teams"||option2 ==="math"||option3 ==="cold"||option4 === "walk"||option5==="warm"); {
+  if (option1 === "Teams"); {
     $("#ruby").show();
+    $("#javascript").hide();
+    $("#python").hide();
   }
-
-});
+    if (option2 === "writting") {
+    $("#javascript").show();
+    $("#ruby").hide();
+    $("#python").hide();
+    elseif (option2 === "math")
+      $("#python").show();
+    };
+  });
 });
