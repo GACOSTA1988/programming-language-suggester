0506847c8327f8a15653b5c7f67890afdf4723ef
George <greg@gregs-MacBook-Pro.local>
2019-10-25 16:06:13 -0700

    complete README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Programming Language suggester
+
+#### This app will help you input criteria to receive a recommendation on the best coding language for you.
+#### By George Acosta  
+
+
+## Setup
+* Clone portfolio
+* Open simple HTTP server in the clone directory
+* Open in browser of choice
+
+## Technologies Used
+1. class
+2. bootstrap
+3. jquery
+
+## Copyright (c) 2019 George Acosta   
+#### This Software is licensed under the MIT license 
--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
 
-  var option1 = $("select#option1").val();
-  var option2 = $("select#option2").val();
-  var option3 = $("select#option3").val();
-  var option4 = $("select#option4").val();
-  var option5 = $("select#option5").val();
+  var option1 = $("#option1").val();
+  var option2 = $("#option2").val();
+  var option3 = $("#option3").val();
+  var option4 = $("#option4").val();
+  var option5 = $("#option5").val();
 
   // option1 = $("select#option1");
   // option2 = $("select#option2");
