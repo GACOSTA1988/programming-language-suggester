42b681f012d89fee0c95ef467373d8d53f504550
George <greg@gregs-MacBook-Pro.local>
2019-10-25 12:59:12 -0700

    troubleshooting error

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-  $("formOne").submit(function(event){
-
-  var question1 = parseInt ();
-  var question2 = parseInt ();
-  var question3 = parseInt ();
-  var question4 = parseInt ();
-  var question5 = parseInt ();
-
-
-
-event.preventDefault();
-});
-});
+// $(document).ready(function(){
+//   $("formOne").submit(function(event){
+//
+//   var question1 = parseInt ($("option#option1").val();
+//   var question2 = parseInt ($("option#option2").val();
+//   var question3 = parseInt ($("option#option3").val();
+//   var question4 = parseInt ($("option#option4").val();
+//   var question5 = parseInt ($("option#option5").val();
+//
+//
+//
+// event.preventDefault();
+// });
+// });
