81bb454471101cc6dfe689dd50f847257af4fca9
George <greg@gregs-MacBook-Pro.local>
2019-10-25 14:00:08 -0700

    add ajvascript and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
-// $(document).ready(function(){
-//   $("formOne").submit(function(event){
-//
-//   var question1 = parseInt ($("option#option1").val();
-//   var question2 = parseInt ($("option#option2").val();
-//   var question3 = parseInt ($("option#option3").val();
-//   var question4 = parseInt ($("option#option4").val();
-//   var question5 = parseInt ($("option#option5").val();
-//
-//
-//
-// event.preventDefault();
-// });
-// });
+$(document).ready(function(){
+  $("form#formOne").submit(function(event){
+  event.preventDefault();
+
+  var question1 = parseInt ($("option#option1").val());
+  var question2 = parseInt ($("option#option2").val());
+  var question3 = parseInt ($("option#option3").val());
+  var question4 = parseInt ($("option#option4").val());
+  var question5 = parseInt ($("option#option5").val());
+
+  if (question1 === "Teams") {
+    $("#ruby").show();
+  }
+
+});
+});
