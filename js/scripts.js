a1359aa6b3ea906d6f1bfcd473168c0dbeb4e853
George <greg@gregs-MacBook-Pro.local>
2019-10-25 11:42:01 -0700

    update html layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-  $("formOne").submit(function(event)
-
+  $("formOne").submit(function(event){
+  cosole.log(hello);
+  var question1 = parseInt($("option#option1").val());
 
 
 
 event.preventDefault();
+});
 });
