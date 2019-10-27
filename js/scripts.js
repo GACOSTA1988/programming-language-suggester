6a653d9c41653fa9e5583cd465b90ee3b1f909da
George <greg@gregs-MacBook-Pro.local>
2019-10-26 17:01:05 -0700

    add return to if statements and cleaned up layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,48 @@
 $(document).ready(function(){
-  // Teams or Solo
-  var option1 = $("#option1").val();
-  // math or writting
-  var option2 = $("#option2").val();
-  // hot or cold
-  var option3 = $("#option3").val();
-  // walk or run
-  var option4 = $("#option4").val();
-  // warm or cold
-  var option5 = $("#option5").val();
-
-  $("#formOne").submit(function(event){
+  $("#formOne").submit(function(event) {
     event.preventDefault();
+    // Teams or Solo
+    var option1 = $("#option1").val();
+    // math or writting
+    var option2 = $("#option2").val();
+    // hot or cold
+    var option3 = $("#option3").val();
+    // walk or run
+    var option4 = $("#option4").val();
+    // warm or cold
+    var option5 = $("#option5").val();
+
     // ERROR CATCHING ALL FIELDS MUST BE FILLED OUT
-    if (option1 === "--"|| option2 ==="--"|| option3 ==="--"|| option4 ==="--"|| option5 ==="--") {
+    if (option1 === "--" || option2 ==="--" || option3 ==="--" || option4 ==="--" || option5 === "--") {
       $("#javascript").hide();
       $("#ruby").hide();
       $("#python").hide();
       $("#completeform").show();
-      return
+      return;
     }
-
+    // TO SHOW RUBY
     if (option1 === "Teams" && option2 === "math") {
       $("#ruby").show();
       $("#javascript").hide();
       $("#python").hide();
       $("#completeform").hide();
+      return;
     }
-
-    if (option2 === "writting") {
+    // TO SHOW JAVASCRIPT
+    if (option1 === "Solo" && option2 === "writting") {
       $("#javascript").show();
       $("#ruby").hide();
       $("#python").hide();
-    } else if (option2 === "math") {
+      $("#completeform").hide();
+      return;
+    }
+    // SHOW PYTHON
+    if (option1 === "Teams" && option2 === "writting") {
       $("#python").show();
       $("#ruby").hide();
       $("#javascript").hide();
+      $("#completeform").hide();
+      return;
     };
-
   });
 });
